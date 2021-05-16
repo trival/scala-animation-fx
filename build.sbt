2ec2d496e6a9aab7fee99b73bbe31c6823abf590
Thomas Gorny <thomas.gorny@pm.me>
2021-05-16 15:12:57 +0200

    init javafx

---
+++ build.sbt
@@ -0,0 +1,39 @@
+scalaVersion := "3.0.0-RC3"
+
+lazy val root = project
+  .in(file("."))
+  .settings(
+    name := "animation-fx",
+    version := "0.1.0",
+    libraryDependencies += "com.lihaoyi" %% "utest" % "0.7.9" % "test",
+    testFrameworks += new TestFramework("utest.runner.Framework"),
+    // Add dependency on ScalaFX library
+    libraryDependencies += "org.scalafx" %% "scalafx" % "16.0.0-R22",
+
+    // Determine OS version of JavaFX binaries
+    libraryDependencies ++= javaFXModules
+      .map(m => "org.openjfx" % s"javafx-$m" % "16" classifier osName),
+    mainClass := Some("hello.ScalaFXHelloWorld"),
+    // Fork a new JVM for 'run' and 'test:run', to avoid JavaFX double initialization problems
+    fork := true
+  )
+
+// ScalaFX setup
+lazy val osName = System.getProperty("os.name") match {
+  case n if n.startsWith("Linux")   => "linux"
+  case n if n.startsWith("Mac")     => "mac"
+  case n if n.startsWith("Windows") => "win"
+  case _                            => throw new Exception("Unknown platform!")
+}
+
+// Add dependency on JavaFX libraries, OS dependent
+lazy val javaFXModules =
+  Seq(
+    "base",
+    "controls",
+//    "fxml",
+    "graphics",
+    "media"
+//    "swing",
+//    "web"
+  )
---
+++ src/main/scala/HelloFX.scala
@@ -0,0 +1,40 @@
+package hello
+
+import scalafx.application.JFXApp3
+import scalafx.application.JFXApp3.PrimaryStage
+import scalafx.geometry.Insets
+import scalafx.scene.Scene
+import scalafx.scene.effect.DropShadow
+import scalafx.scene.layout.HBox
+import scalafx.scene.paint.Color.*
+import scalafx.scene.paint.*
+import scalafx.scene.text.Text
+
+import scala.language.implicitConversions
+
+object ScalaFXHelloWorld extends JFXApp3:
+
+  override def start(): Unit =
+    stage = new PrimaryStage:
+      onCloseRequest = (e) => println("exiting°")
+      title = "ScalaFX Hello World"
+      scene = new Scene:
+        fill = Color.rgb(38, 38, 38)
+        content = new HBox:
+          padding = Insets(50, 80, 50, 80)
+          children = Seq(
+            new Text:
+              text = "Scala"
+              style = "-fx-font: normal bold 100pt sans-serif"
+              fill = new LinearGradient(endX = 0, stops = Stops(Red, DarkRed))
+            ,
+            new Text:
+              text = "FX"
+              style = "-fx-font: italic bold 100pt sans-serif"
+              fill =
+                new LinearGradient(endX = 0, stops = Stops(White, DarkGray))
+              effect = new DropShadow:
+                color = DarkGray
+                radius = 15
+                spread = 0.25
+          )
