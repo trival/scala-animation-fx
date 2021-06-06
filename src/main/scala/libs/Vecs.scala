483081c750ddef63ef5b760e5cf406afc58913fe
Thomas Gorny <thomas.gorny@pm.me>
2021-06-06 11:06:17 +0200

    implement more vector ops

--- src/main/scala/libs/Vecs.scala
+++ src/main/scala/libs/Vecs.scala
@@ -1,8 +1,89 @@
 package libs.vecs
 
-case class Vec2(x: Double, y: Double) {
-  def +(other: Vec2) = Vec2(x + other.x, y + other.y)
-}
+import scala.language.implicitConversions
+
+
+// === Vec2 ===
+
+case class Vec2(x: Double, y: Double):
+  inline def apply(a: Double) = Vec2(a, a)
+  inline def +(v: Vec2) = Vec2(x + v.x, y + v.y)
+  inline def +(a: Double) = Vec2(a + x, a + y)
+  inline def *(v: Vec2) = Vec2(x * v.x, y * v.y)
+  inline def *(a: Double) = Vec2(a * x, a * y)
+
+  inline infix def dot(v: Vec2): Double = x * v.x + y * v.y
+end Vec2
+
+case object Vec2:
+  inline def apply(a: Double): Vec2 = Vec2(a, a)
+end Vec2
+
+// === Vec3 ===
+
+case class Vec3(x: Double, y: Double, z: Double):
+  inline def +(v: Vec3) = Vec3(x + v.x, y + v.y, z + v.z)
+  inline def +(a: Double) = Vec3(a + x, a + y, a + z)
+  inline def *(v: Vec3) = Vec3(x * v.x, y * v.y, z * v.z)
+  inline def *(a: Double) = Vec3(a * x, a * y, a * z)
+  inline infix def dot(v: Vec3): Double = x * v.x + y * v.y + z * v.z
+end Vec3
+
+case object Vec3:
+  inline def apply(a: Double): Vec3 = Vec3(a, a, a)
+end Vec3
+
+// === Vec4 ===
+
+case class Vec4(x: Double, y: Double, z: Double, w: Double):
+  inline def +(v: Vec4) = Vec4(x + v.x, y + v.y, z + v.z, w + v.w)
+  inline def +(a: Double) = Vec4(a + x, a + y, a + z, w + a)
+  inline def *(v: Vec4) = Vec4(x * v.x, y * v.y, z * v.z, w * v.w)
+  inline def *(a: Double) = Vec4(a * x, a * y, a * z, w * a)
+  inline infix def dot(v: Vec4): Double = x * v.x + y * v.y + z * v.z + w * v.w
+end Vec4
+
+case object Vec4:
+  inline def apply(a: Double): Vec4 = Vec4(a, a, a, a)
+end Vec4
+
+// === Extensions ===
+
+extension (d: Double) 
+  inline def +(v: Vec2) = v + d
+  inline def +(v: Vec3) = v + d
+  inline def +(v: Vec4) = v + d
+
+  inline def *(v: Vec2) = v * d
+  inline def *(v: Vec3) = v * d
+  inline def *(v: Vec4) = v * d
+end extension
+
+// === Conversions ===
 
 given Conversion[(Double, Double), Vec2] with
-  def apply(t: (Double, Double)) = Vec2(t._1, t._2)
+  inline def apply(t: (Double, Double)) = Vec2(t._1, t._2)
+
+given Conversion[(Int, Int), Vec2] with
+  inline def apply(t: (Int, Int)) = Vec2(t._1, t._2)
+
+given Conversion[Vec2, (Double, Double)] with
+  inline def apply(v: Vec2) = (v.x, v.y)
+
+given Conversion[(Double, Double, Double), Vec3] with
+  inline def apply(t: (Double, Double, Double)) = Vec3(t._1, t._2, t._3)
+
+given Conversion[(Int, Int, Int), Vec3] with
+  inline def apply(t: (Int, Int, Int)) = Vec3(t._1, t._2, t._3)
+
+given Conversion[Vec3, (Double, Double, Double)] with
+  inline def apply(v: Vec3) = (v.x, v.y, v.z)
+
+given Conversion[(Double, Double, Double, Double), Vec4] with
+  inline def apply(t: (Double, Double, Double, Double)) = Vec4(t._1, t._2, t._3, t._4)
+
+given Conversion[(Int, Int, Int, Int), Vec4] with
+  inline def apply(t: (Int, Int, Int, Int)) = Vec4(t._1, t._2, t._3, t._4)
+
+given Conversion[Vec4, (Double, Double, Double, Double)] with
+  inline def apply(v: Vec4) = (v.x, v.y, v.z, v.w)
--- src/test/scala/libs/Vecs.scala
+++ src/test/scala/libs/Vecs.scala
@@ -7,12 +7,95 @@ object VecsTests extends TestSuite:
   val tests = Tests {
 
     test("Vec2") {
+      test("have unary constructor") {
+        Vec2(2) ==> Vec2(2, 2)
+      }
+
       test("can add") {
         Vec2(2, 3) + Vec2(3, 4) ==> Vec2(5, 7)
+        Vec2(2, 3) + 3 ==> Vec2(5, 6)
+      }
+
+      test("can multipy") {
+        Vec2(2, 3) * Vec2(3, 4) ==> Vec2(6, 12)
+        Vec2(2, 3) * 3 ==> Vec2(6, 9)
+      }
+
+      test("has dot") {
+        Vec2(1, 2).dot(Vec2(2, 3)) ==> 8.0
+        (Vec2(1, 2) dot Vec2(2, 3)) ==> 8.0
+        val v1 = Vec2(1, 2)
+        val v2 = Vec2(2, 3)
+        val v3 = v1 dot v2
+        val v4 = Vec2(1, 2) dot Vec2(2, 3)
+        v3 ==> 8
+        v4 ==> 8
       }
 
       test("convert tuples to vecs") {
-        Vec2(2, 3) + (3.0, 4.0) ==> Vec2(5, 7)
+        (2, 3) + (3, 4) ==> Vec2(5, 7)
+        (2, 3) + 3 ==> Vec2(5, 6)
+        (2, 3) * 2 ==> Vec2(4, 6)
+      }
+    }
+
+    test("Vec3") {
+      test("have unary constructor") {
+        Vec3(2) ==> Vec3(2, 2, 2)
+      }
+
+      test("can add") {
+        Vec3(2, 3, 4) + Vec3(3, 4, 5) ==> Vec3(5, 7, 9)
+        Vec3(2, 3, 4) + 3 ==> Vec3(5, 6, 7)
+        3 + (2, 3, 4) ==> Vec3(5, 6, 7)
+      }
+
+      test("can multiply") {
+        Vec3(2, 3, 4) * Vec3(1, 2, 3) ==> Vec3(2, 6, 12)
+        Vec3(2, 3, 4) * 2 ==> Vec3(4, 6, 8)
+        2 * (2, 3, 4) ==> Vec3(4, 6, 8)
+      }
+
+      test("has dot") {
+        val v1 = Vec3(1, 2, 3)
+        val v2 = Vec3(2, 3, 4)
+        val v3 = v1 dot v2
+        v3 ==> 20
+      }
+
+      test("convert tuples to vecs") {
+        (2, 3, 4) + (3, 4, 5) ==> Vec3(5, 7, 9)
+        (2, 3, 4) + 3 ==> Vec3(5, 6, 7)
+      }
+    }
+
+    test("Vec4") {
+      test("have unary constructor") {
+        Vec4(2) ==> Vec4(2, 2, 2, 2)
+      }
+
+      test("can add") {
+        Vec4(2, 3, 4, 5) + Vec4(3, 4, 5, 6) ==> Vec4(5, 7, 9, 11)
+        Vec4(2, 3, 4, 5) + 3 ==> Vec4(5, 6, 7, 8)
+        3 + (2, 3, 4, 5) ==> Vec4(5, 6, 7, 8)
+      }
+
+      test("can multiply") {
+        Vec4(2, 3, 4, 5) * Vec4(1, 2, 3, 4) ==> Vec4(2, 6, 12, 20)
+        Vec4(2, 3, 4, 5) * 2 ==> Vec4(4, 6, 8, 10)
+        2 * (2, 3, 4, 5) ==> Vec4(4, 6, 8, 10)
+      }
+
+      test("has dot") {
+        val v1 = Vec4(1, 2, 3, 4)
+        val v2 = Vec4(2, 3, 4, 5)
+        val v3 = v1 dot v2
+        v3 ==> 40
+      }
+
+      test("convert tuples to vecs") {
+        (2, 3, 4, 5) + (3, 4, 5, 6) ==> Vec4(5, 7, 9, 11)
+        (2, 3, 4, 5) + 3 ==> Vec4(5, 6, 7, 8)
       }
     }
   }
